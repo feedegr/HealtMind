ac94e0630c9d8907a5c660febfb991f4df0304ea
feede.gr <feedecs@hotmail.com>
2023-06-16 01:28:23 -0300

    v1.0 final

--- src/hooks/Resultado.jsx
+++ src/hooks/Resultado.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/preguntas.css";
 
-export const Resultado = ({ sumaindices }) => {
+export const Resultado = ({ sumaindices, respuestasCompletas }) => {
   const [mostrar, setMostrar] = useState(false);
-
-  // Puntuación de 0 a 4: Ausencia o mínima depresión.
-  // Puntuación de 5 a 9: Depresión leve.
-  // Puntuación de 10 a 14: Depresión moderada.
-  // Puntuación de 15 a 19: Depresión moderadamente severa.
-  // Puntuación de 20 o más: Depresión grave.
+  const [bloqueado, setBloqueado] = useState(false);
 
   const resultadoFinal = () => {
     if (sumaindices >= 0 && sumaindices <= 4) {
@@ -24,24 +19,40 @@ export const Resultado = ({ sumaindices }) => {
     }
   };
 
+  const todasLasPreguntasRespondidas =
+    Object.keys(respuestasCompletas).length === 9;
+
+  useEffect(() => {
+    setBloqueado(todasLasPreguntasRespondidas);
+  }, [respuestasCompletas]);
+
+  console.log(respuestasCompletas);
+  console.log(bloqueado);
+
   const handleclick = () => {
     setMostrar(true);
   };
 
   return (
     <div className="container-fluid mt-5 text-center">
-      
-        <button className="btn btn-lg result mt-3 mb-3" onClick={handleclick}>
-          Resultado
-        </button>
+      <button
+        className="btn btn-lg result mt-3 mb-3"
+        onClick={handleclick}
+        disabled={!bloqueado}
+      >
+        Resultado
+      </button>
 
-        {mostrar && (
-          <>
-            <p className="text-center contpreguntas">{resultadoFinal()}</p>
-          </>
-        )}
-      
-        <p className="contpreguntas"> Es un test informativo, consulte a un profesional. </p>
+      {mostrar && (
+        <>
+          <p className="text-center contfinal">{resultadoFinal()}</p>
+        </>
+      )}
+
+      <p className="contpreguntas">
+        {" "}
+        Es un test informativo, consulte a un profesional.{" "}
+      </p>
     </div>
   );
 };
